e1382e39f19304483085fb5ea8b80d97e44143a0
Zwissig Noé <54308994+Witex15@users.noreply.github.com>
2024-02-01 12:02:58 +0100

    feat: add sum

--- src/Cart/Cart.js
+++ src/Cart/Cart.js
@@ -2,6 +2,7 @@ const EmptyCartException = require("./EmptyCartException");
 module.exports = class Cart {
     //region private attributes
     #items;
+    #total;
     //endregion private attributes
 
     //region public methods
@@ -14,8 +15,18 @@ module.exports = class Cart {
         }
         return this.#items;
     }
+    get total() {
+        return this.getSum();
+    }
     //endregion public methods
 
     //region private methods
+    getSum() {
+        let sum = 0;
+        for (let i = 0; i < this.items.length; i++) {
+            sum += this.items[i].total;
+        }
+        return sum;
+    }
     //endregion private methods
 }
